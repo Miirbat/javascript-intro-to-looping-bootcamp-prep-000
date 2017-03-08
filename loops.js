176e4512767314998d63cc6fb057f192bde9c59e
Mirna Plakalovic <mirna.plakalovic@gmail.com>
2017-03-08 21:20:39 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,28 +1,32 @@
-function forLoop (array) {
+function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    array.push("I am ${i} strange loop${i === 0 ? '' : 's'}.");
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
   }
+
   return array
 }
 
 
-
 function whileLoop(n) {
-//let n = 100;
   while (n > 0) {
     console.log(--n)
   }
-return "done";
-}
 
+  return 'done'
+}
 
 function doWhileLoop(array) {
-
   function maybeTrue() {
-  return
-}
+    return Math.random() >= 0.5
+  }
 
-do {
-  array.push('')
-} while (maybeTrue());
+  do {
+    array = array.slice(1)
+  } while (array.length > 0, maybeTrue())
+
+  return array
 }
